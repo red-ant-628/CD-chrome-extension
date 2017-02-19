322857b0a418c5bb209ff2ae1c7664047116f43e
red-ant-628 <red.ant.628@gmail.com>
2017-02-19 09:18:25 -0600

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,10 @@
 var images = document.getElementsByTagName('img');
 for (var i = 0, l = images.length; i < l; i++) {
   images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
-}
+  
+while (1 == 1) {
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+ setTimeout(5000)
+  }

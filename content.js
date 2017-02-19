c602d5c688047dccbd35a66d3cfd4ee4e0c6df46
red-ant-628 <red.ant.628@gmail.com>
2017-02-19 00:43:23 -0600

    Update content.js

--- content.js
+++ content.js
@@ -2,17 +2,3 @@ var images = document.getElementsByTagName('img');
 for (var i = 0, l = images.length; i < l; i++) {
   images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
 }
-var string = '<!DOCTYPE html><html><head></head><body>content</body></html>';
-var doc = new DOMParser().parseFromString(string, 'text/html');
-doc.body.innerHTML;
-wordcount = string.length;
-
-$ npm install --save saurus
-
-for (wordcount) {
-var thesaurus = require('saurus');
-
-thesaurus(string(1).then(matches => console.log(matches));
- 
-document.getElementById("output").innerHTML=thesaurus;
-}

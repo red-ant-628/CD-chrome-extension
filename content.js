b02f706323b4796982b710a34d2d1f89cae9b19c
red-ant-628 <red.ant.628@gmail.com>
2017-02-19 08:02:45 -0600

    Update content.js

--- content.js
+++ content.js
@@ -2,3 +2,7 @@ var images = document.getElementsByTagName('img');
 for (var i = 0, l = images.length; i < l; i++) {
   images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
 }
+
+var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+console.log(firstHref);

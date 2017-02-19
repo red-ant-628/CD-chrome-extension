15acdc3f909f26f91a8365c9961bef3a10fc24a3
red-ant-628 <red.ant.628@gmail.com>
2017-02-19 08:16:35 -0600

    Update content.js

--- content.js
+++ content.js
@@ -2,17 +2,3 @@ var images = document.getElementsByTagName('img');
 for (var i = 0, l = images.length; i < l; i++) {
   images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
 }
-
-var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-console.log(firstHref);
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-      console.log(firstHref);
-    }
-  }
-);

a6021f808eef33ca7b8b5150e947012407fa3777
red-ant-628 <red.ant.628@gmail.com>
2017-02-19 08:07:26 -0600

    Update content.js

--- content.js
+++ content.js
@@ -6,3 +6,13 @@ for (var i = 0, l = images.length; i < l; i++) {
 var firstHref = $("a[href^='http']").eq(0).attr("href");
 
 console.log(firstHref);
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+    }
+  }
+);
